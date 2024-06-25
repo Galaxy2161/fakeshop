9623df8dfc522fa2c1317d41e49465b6af8e32d5
Anthony <tony@Anthonys-Air.lan>
2024-06-24 21:50:05 -0400

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './Global.css'
+import { Header } from './containers/Header'
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom'
+import { ProductListing } from './containers/ProductListing'
+import { ProductDetails } from './containers/ProductDetails'
+import './index.css'
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+     <Router>
+      <Header/>
+      <Routes>
+         <Route path='/' element={<ProductListing/>}/>
+         <Route path='/product/:productId' element={<ProductDetails/>}/>
+         <Route>404 Not Found</Route>
+      </Routes>
+     </Router>
+  )
 }
-
-export default App;
+ 
